a72ce93d9859ed3fc9dcb93102b69a0d2085d531
birmacher <birmacher@gmail.com>
2015-10-29 11:10:12 +0000

    using ./gradlew yml updated

--- step.sh
+++ step.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-set -e
 
-if [ ! -f "gradlew" ]; then
+if [ ! -f "gradlew" ] ; then
     echo "\e[31mDidn't find a gradlew file in the root directory\e[0m"
     exit 0
 fi
 
+./gradlew ${unit_test_task} ${unit_test_flags}
+return_code=$?
 
-./gradlew test --continue
+if [ "${return_code}" -eq "0" ] ; then
+	envman add --key "BITRISE_GRADLE_TEST_RESULT" --value "succeeded"
+else
+	envman add --key "BITRISE_GRADLE_TEST_RESULT" --value "failed"
+fi
+
+echo "BITRISE_GRADLE_TEST_RESULT added to the environment with value ${BITRISE_GRADLE_TEST_RESULT}"
+exit return_code
+
