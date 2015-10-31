3113ce9c2226c05da6c5f84ffe33c59b819c69a9
birmacher <birmacher@gmail.com>
2015-10-31 12:50:56 +0000

    support for custom gradlew path

--- step.sh
+++ step.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-if [ ! -f "gradlew" ] ; then
+if [ -z "${gradlew_file_path}" ] ; then
+	printf "\e[31gradlew_file_path was not defined\e[0m\n"
+    exit 1
+fi
+
+if [ ! -f "${gradlew_file_path}" ] ; then
     printf "\e[31mDidn't find a gradlew file in the root directory\e[0m\n"
     exit 1
 fi
@@ -10,15 +15,17 @@ if [ -z "${unit_test_task}" ] ; then
     exit 1
 fi
 
-./gradlew ${unit_test_task} ${unit_test_flags}
+echo "$" ${gradlew_file_path} ${unit_test_task} ${unit_test_flags}
+${gradlew_file_path} ${unit_test_task} ${unit_test_flags}
 return_code=$?
 
 if [ "${return_code}" -eq "0" ] ; then
 	envman add --key "BITRISE_GRADLE_TEST_RESULT" --value "succeeded"
+	echo "BITRISE_GRADLE_TEST_RESULT added to the environment with value succeeded"
 else
 	envman add --key "BITRISE_GRADLE_TEST_RESULT" --value "failed"
+	echo "BITRISE_GRADLE_TEST_RESULT added to the environment with value failed"
 fi
 
-echo "BITRISE_GRADLE_TEST_RESULT added to the environment with value ${return_code}"
 exit ${return_code}
 
