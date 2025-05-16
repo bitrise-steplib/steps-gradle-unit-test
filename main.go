1645cdccf3b5742fa5e79fd97d2eb5ef6667861e
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-05-16 10:39:16 +0200

    Upgrade to v2 libs

--- main.go
+++ main.go
@@ -1,96 +1,95 @@
 package main
 
 import (
-	"crypto/md5"
-	"errors"
 	"fmt"
-	"io"
 	"os"
-	"strings"
 
-	"github.com/bitrise-io/go-android/cache"
+	"github.com/bitrise-io/go-android/v2/cache"
 	utilscache "github.com/bitrise-io/go-steputils/cache"
-	"github.com/bitrise-io/go-utils/command"
-	"github.com/bitrise-io/go-utils/log"
-	"github.com/bitrise-io/go-utils/pathutil"
-	"github.com/bitrise-io/go-steputils/input"
-	shellquote "github.com/kballard/go-shellquote"
+	"github.com/bitrise-io/go-steputils/v2/export"
+	"github.com/bitrise-io/go-steputils/v2/stepconf"
+	"github.com/bitrise-io/go-utils/v2/command"
+	"github.com/bitrise-io/go-utils/v2/env"
+	"github.com/bitrise-io/go-utils/v2/log"
+	"github.com/bitrise-io/go-utils/v2/pathutil"
+	"github.com/kballard/go-shellquote"
 )
 
-// ConfigsModel ...
-type ConfigsModel struct {
-	GradleFile    string
-	UnitTestTasks string
-	GradlewPath   string
-	UnitTestFlags string
-	DeployDir     string
-	CacheLevel    string
+type Inputs struct {
+	GradleFile    string `env:"gradle_file"`
+	UnitTestTasks string `env:"unit_test_task,required"`
+	GradlewPath   string `env:"gradlew_file_path,required"`
+	UnitTestFlags string `env:"unit_test_flags"`
+	DeployDir     string `env:"BITRISE_DEPLOY_DIR"`
+	CacheLevel    string `env:"cache_level,opt[all,only deps,none]"`
 }
 
-func createConfigsModelFromEnvs() ConfigsModel {
-	return ConfigsModel{
-		GradleFile:    os.Getenv("gradle_file"),
-		UnitTestTasks: os.Getenv("unit_test_task"),
-		GradlewPath:   os.Getenv("gradlew_file_path"),
-		UnitTestFlags: os.Getenv("unit_test_flags"),
-		DeployDir:     os.Getenv("BITRISE_DEPLOY_DIR"),
-		CacheLevel:    os.Getenv("cache_level"),
+func main() {
+	// Setup dependencies
+	logger := log.NewLogger()
+	envRepo := env.NewRepository()
+	stepInputParser := stepconf.NewInputParser(envRepo)
+	pathChecker := pathutil.NewPathChecker()
+	cmdFactory := command.NewFactory(envRepo)
+	outputExporter := export.NewExporter(cmdFactory)
+
+	// Parse inputs
+	var inputs Inputs
+	if err := stepInputParser.Parse(&inputs); err != nil {
+		failF(logger, "issue with input: %s", err)
+	}
+
+	stepconf.Print(inputs)
+	logger.Println()
+
+	if err := validateInputs(pathChecker, inputs); err != nil {
+		failF(logger, "issue with input: %s", err)
+	}
+
+	// Run gradle task
+	err := os.Chmod(inputs.GradlewPath, 0770)
+	if err != nil {
+		failF(logger, "Failed to add executable permission on gradlew file (%s): %s", inputs.GradlewPath, err)
+	}
+
+	fmt.Println()
+	logger.Infof("Running gradle task...")
+	if err := runGradleTask(cmdFactory, logger, inputs.GradlewPath, inputs.GradleFile, inputs.UnitTestTasks, inputs.UnitTestFlags); err != nil {
+		logger.Errorf("Gradle task failed: %s", err)
+
+		if err := outputExporter.ExportOutput("BITRISE_GRADLE_TEST_RESULT", "failed"); err != nil {
+			logger.Warnf("Failed to export environment: %s: %s", "BITRISE_GRADLE_TEST_RESULT", err)
+		}
+
+		os.Exit(1)
+	}
+
+	if err := outputExporter.ExportOutput("BITRISE_GRADLE_TEST_RESULT", "succeeded"); err != nil {
+		logger.Warnf("Failed to export environment: %s: %s", "BITRISE_GRADLE_TEST_RESULT", err)
+	}
+
+	// Collecting caches
+	logger.Infof("Collecting cache:")
+	const defaultProjectRoot = "."
+
+	if err := cache.Collect(defaultProjectRoot, utilscache.Level(inputs.CacheLevel), cmdFactory); err != nil {
+		logger.Warnf("Failed to collect cache: %s", err)
 	}
 }
 
-func (configs ConfigsModel) print() {
-	log.Infof("Configs:")
-	log.Printf("- GradleFile: %s", configs.GradleFile)
-	log.Printf("- UnitTestTasks: %s", configs.UnitTestTasks)
-	log.Printf("- GradlewPath: %s", configs.GradlewPath)
-	log.Printf("- UnitTestFlags: %s", configs.UnitTestFlags)
-
-	log.Printf("- DeployDir: %s", configs.DeployDir)
-	log.Printf("- CacheLevel: %s", configs.CacheLevel)
-}
-
-func (configs ConfigsModel) validate() (string, error) {
-	if configs.GradleFile != "" {
-		if exist, err := pathutil.IsPathExists(configs.GradleFile); err != nil {
-			return "", fmt.Errorf("Failed to check if GradleFile exist at: %s, error: %s", configs.GradleFile, err)
+func validateInputs(pathChecker pathutil.PathChecker, inputs Inputs) error {
+	if inputs.GradleFile != "" {
+		if exist, err := pathChecker.IsPathExists(inputs.GradleFile); err != nil {
+			return fmt.Errorf("failed to check if GradleFile exist at %s: %w", inputs.GradleFile, err)
 		} else if !exist {
-			return "", fmt.Errorf("GradleFile not exist at: %s", configs.GradleFile)
+			return fmt.Errorf("gradle file not exist at: %s", inputs.GradleFile)
 		}
 	}
 
-	if configs.UnitTestTasks == "" {
-		return "", errors.New("no UnitTestTasks parameter specified")
-	}
-
-	if configs.GradlewPath == "" {
-		explanation := `
-Using a Gradle Wrapper (gradlew) is required, as the wrapper is what makes sure
-that the right Gradle version is installed and used for the build.
-
-You can find more information about the Gradle Wrapper (gradlew),
-and about how you can generate one (if you would not have one already
-in the official guide at: https://docs.gradle.org/current/userguide/gradle_wrapper.html`
-
-		return explanation, errors.New("no GradlewPath parameter specified")
-	}
-	if exist, err := pathutil.IsPathExists(configs.GradlewPath); err != nil {
-		return "", fmt.Errorf("Failed to check if GradlewPath exist at: %s, error: %s", configs.GradlewPath, err)
-	} else if !exist {
-		return "", fmt.Errorf("GradlewPath not exist at: %s", configs.GradlewPath)
-	}
-
-	if err := input.ValidateIfNotEmpty(configs.CacheLevel); err != nil {
-		return "", fmt.Errorf("CacheLevel, error: %s", err)
-	}
-
-	if err := input.ValidateWithOptions(configs.CacheLevel, "all", "only deps", "none"); err != nil {
-		return "", fmt.Errorf("CacheLevel, error: %s", err)
-	}
-
-	return "", nil
+	return nil
 }
 
-func runGradleTask(gradleTool, buildFile, tasks, options string) error {
+func runGradleTask(cmdFactory command.Factory, logger log.Logger, gradleTool, buildFile, tasks, options string) error {
 	optionSlice, err := shellquote.Split(options)
 	if err != nil {
 		return err
@@ -101,97 +100,25 @@ func runGradleTask(gradleTool, buildFile, tasks, options string) error {
 		return err
 	}
 
-	cmdSlice := []string{gradleTool}
+	var args []string
 	if buildFile != "" {
-		cmdSlice = append(cmdSlice, "--build-file", buildFile)
+		args = append(args, "--build-file", buildFile)
 	}
-	cmdSlice = append(cmdSlice, taskSlice...)
-	cmdSlice = append(cmdSlice, optionSlice...)
+	args = append(args, taskSlice...)
+	args = append(args, optionSlice...)
 
-	log.Donef("$ %s", command.PrintableCommandArgs(false, cmdSlice))
+	cmd := cmdFactory.Create(gradleTool, args, &command.Opts{
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	})
+
+	logger.Donef("$ %s", cmd.PrintableCommandArgs())
 	fmt.Println()
-
-	cmd, err := command.NewFromSlice(cmdSlice)
-	if err != nil {
-		return fmt.Errorf("failed to create command, error: %s", err)
-	}
-
-	cmd.SetStdout(os.Stdout)
-	cmd.SetStderr(os.Stderr)
 
 	return cmd.Run()
 }
 
-func exportEnvironmentWithEnvman(keyStr, valueStr string) error {
-	cmd := command.New("envman", "add", "--key", keyStr)
-	cmd.SetStdin(strings.NewReader(valueStr))
-	return cmd.Run()
-}
-
-func computeMD5String(filePath string) (string, error) {
-	f, err := os.Open(filePath)
-	if err != nil {
-		return "", err
-	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			log.Errorf("Failed to close file(%s), error: %s", filePath, err)
-		}
-	}()
-
-	h := md5.New()
-	if _, err := io.Copy(h, f); err != nil {
-		return "", err
-	}
-
-	return fmt.Sprintf("%x", h.Sum(nil)), nil
-}
-
-func main() {
-	configs := createConfigsModelFromEnvs()
-
-	fmt.Println()
-	configs.print()
-
-	if explanation, err := configs.validate(); err != nil {
-		fmt.Println()
-		log.Errorf("Issue with input: %s", err)
-		fmt.Println()
-
-		if explanation != "" {
-			fmt.Println(explanation)
-			fmt.Println()
-		}
-
-		os.Exit(1)
-	}
-
-	err := os.Chmod(configs.GradlewPath, 0770)
-	if err != nil {
-		log.Errorf("Failed to add executable permission on gradlew file (%s), error: %s", configs.GradlewPath, err)
-		os.Exit(1)
-	}
-
-	fmt.Println()
-	log.Infof("Running gradle task...")
-	if err := runGradleTask(configs.GradlewPath, configs.GradleFile, configs.UnitTestTasks, configs.UnitTestFlags); err != nil {
-		log.Errorf("Gradle task failed, error: %s", err)
-
-		if err := exportEnvironmentWithEnvman("BITRISE_GRADLE_TEST_RESULT", "failed"); err != nil {
-			log.Warnf("Failed to export environment: %s, error: %s", "BITRISE_GRADLE_TEST_RESULT", err)
-		}
-
-		os.Exit(1)
-	}
-
-	// Collecting caches
-	log.Infof("Collecting cache:")
-	const defaultProjectRoot = "."
-	if warning := cache.Collect(defaultProjectRoot, utilscache.Level(configs.CacheLevel)); warning != nil {
-		log.Warnf("%s", warning)
-	}
-
-	if err := exportEnvironmentWithEnvman("BITRISE_GRADLE_TEST_RESULT", "succeeded"); err != nil {
-		log.Warnf("Failed to export environment: %s, error: %s", "BITRISE_GRADLE_TEST_RESULT", err)
-	}
+func failF(logger log.Logger, format string, args ...interface{}) {
+	logger.Errorf(format, args...)
+	os.Exit(1)
 }
--- vendor/github.com/bitrise-io/go-android/v2/cache/gradle.go
+++ vendor/github.com/bitrise-io/go-android/v2/cache/gradle.go
@@ -7,9 +7,9 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/errorutil"
 	"github.com/bitrise-io/go-utils/pathutil"
+	"github.com/bitrise-io/go-utils/v2/command"
 	"github.com/hashicorp/go-version"
 )
 
@@ -38,7 +38,7 @@ func parseGradleVersion(out string) (string, error) {
 	return matches[1], nil
 }
 
-func projectGradleVersion(projectPth string) (string, error) {
+func projectGradleVersion(projectPth string, cmdFactory command.Factory) (string, error) {
 	gradlewPth := filepath.Join(projectPth, "gradlew")
 	exist, err := pathutil.IsPathExists(gradlewPth)
 	if err != nil {
@@ -48,8 +48,8 @@ func projectGradleVersion(projectPth string) (string, error) {
 		return "", fmt.Errorf("no gradlew found at: %s", gradlewPth)
 	}
 
-	versionCmd := command.New("./gradlew", "-version")
-	versionCmd.SetDir(filepath.Dir(gradlewPth))
+	versionCmdOpts := command.Opts{Dir: filepath.Dir(gradlewPth)}
+	versionCmd := cmdFactory.Create("./gradlew", []string{"-version"}, &versionCmdOpts)
 	out, err := versionCmd.RunAndReturnTrimmedCombinedOutput()
 	if err != nil {
 		if errorutil.IsExitStatusError(err) {
