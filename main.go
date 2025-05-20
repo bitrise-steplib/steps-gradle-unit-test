32e6681801913a75cff01a2821e9f4779acabe07
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2025-05-20 10:31:42 +0200

    Reshuffle step inputs for project root dir based configuration (#24)
    * Reshuffle step inputs for project root dir based configuration
    
    * Set gradlew command workdir
    
    * Test project in a subdir
    
    * Fix yaml lint issue
    
    * Fix project in subdir test
    
    * Replace custom git clone script with simple-git-clone step
    
    * Cleanup bitrise.yml
    
    * bitrise.yml cleanup
    
    * Fix gradle build script rel path issue
    
    * Code cleanup
    
    * Run custom test task in test-project-in-sub-dir
    
    * Remove legacy cache support
    
    * Code cleanup

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
-	"github.com/bitrise-io/go-android/v2/cache"
-	utilscache "github.com/bitrise-io/go-steputils/cache"
 	"github.com/bitrise-io/go-steputils/v2/export"
 	"github.com/bitrise-io/go-steputils/v2/stepconf"
 	"github.com/bitrise-io/go-utils/v2/command"
@@ -16,45 +15,43 @@ import (
 )
 
 type Inputs struct {
-	GradleFile    string `env:"gradle_file"`
-	UnitTestTasks string `env:"unit_test_task,required"`
-	GradlewPath   string `env:"gradlew_file_path,required"`
-	UnitTestFlags string `env:"unit_test_flags"`
-	DeployDir     string `env:"BITRISE_DEPLOY_DIR"`
-	CacheLevel    string `env:"cache_level,opt[all,only deps,none]"`
+	ProjectRootDir        string `env:"project_root_dir,dir"`
+	TestTasks             string `env:"test_task,required"`
+	GradlewCommandFlags   string `env:"gradlew_command_flags"`
+	GradleBuildScriptPath string `env:"gradle_build_script_path"`
+}
+
+type Configs struct {
+	ProjectRootDir                string
+	GradlewPath                   string
+	TestTasks                     []string
+	GradlewCommandFlags           []string
+	GradleBuildScriptRelativePath string
 }
 
 func main() {
 	// Setup dependencies
 	logger := log.NewLogger()
 	envRepo := env.NewRepository()
-	stepInputParser := stepconf.NewInputParser(envRepo)
+	inputParser := stepconf.NewInputParser(envRepo)
 	pathChecker := pathutil.NewPathChecker()
 	cmdFactory := command.NewFactory(envRepo)
 	outputExporter := export.NewExporter(cmdFactory)
 
 	// Parse inputs
-	var inputs Inputs
-	if err := stepInputParser.Parse(&inputs); err != nil {
-		failF(logger, "issue with input: %s", err)
-	}
-
-	stepconf.Print(inputs)
-	logger.Println()
-
-	if err := validateInputs(pathChecker, inputs); err != nil {
-		failF(logger, "issue with input: %s", err)
+	config, err := processConfig(inputParser, pathChecker, logger)
+	if err != nil {
+		failF(logger, "Failed to process config: %s", err)
 	}
 
 	// Run gradle task
-	err := os.Chmod(inputs.GradlewPath, 0770)
-	if err != nil {
-		failF(logger, "Failed to add executable permission on gradlew file (%s): %s", inputs.GradlewPath, err)
+	if err := os.Chmod(config.GradlewPath, 0770); err != nil {
+		failF(logger, "Failed to add executable permission on gradlew file (%s): %s", config.GradlewPath, err)
 	}
 
 	fmt.Println()
 	logger.Infof("Running gradle task...")
-	if err := runGradleTask(cmdFactory, logger, inputs.GradlewPath, inputs.GradleFile, inputs.UnitTestTasks, inputs.UnitTestFlags); err != nil {
+	if err := runGradleTask(cmdFactory, logger, config.ProjectRootDir, config.TestTasks, config.GradleBuildScriptRelativePath, config.GradlewCommandFlags); err != nil {
 		logger.Errorf("Gradle task failed: %s", err)
 
 		if err := outputExporter.ExportOutput("BITRISE_GRADLE_TEST_RESULT", "failed"); err != nil {
@@ -67,49 +64,65 @@ func main() {
 	if err := outputExporter.ExportOutput("BITRISE_GRADLE_TEST_RESULT", "succeeded"); err != nil {
 		logger.Warnf("Failed to export environment: %s: %s", "BITRISE_GRADLE_TEST_RESULT", err)
 	}
-
-	// Collecting caches
-	logger.Infof("Collecting cache:")
-	const defaultProjectRoot = "."
-
-	if err := cache.Collect(defaultProjectRoot, utilscache.Level(inputs.CacheLevel), cmdFactory); err != nil {
-		logger.Warnf("Failed to collect cache: %s", err)
-	}
 }
 
-func validateInputs(pathChecker pathutil.PathChecker, inputs Inputs) error {
-	if inputs.GradleFile != "" {
-		if exist, err := pathChecker.IsPathExists(inputs.GradleFile); err != nil {
-			return fmt.Errorf("failed to check if GradleFile exist at %s: %w", inputs.GradleFile, err)
+func processConfig(inputParser stepconf.InputParser, pathChecker pathutil.PathChecker, logger log.Logger) (*Configs, error) {
+	var inputs Inputs
+	if err := inputParser.Parse(&inputs); err != nil {
+		return nil, fmt.Errorf("issue with input: %s", err)
+	}
+
+	stepconf.Print(inputs)
+	logger.Println()
+
+	var gradleBuildScriptPath string
+	if inputs.GradleBuildScriptPath != "" {
+		gradleBuildScriptPath = filepath.Join(inputs.ProjectRootDir, inputs.GradleBuildScriptPath)
+		if exist, err := pathChecker.IsPathExists(gradleBuildScriptPath); err != nil {
+			return nil, fmt.Errorf("failed to check if gradle build file exist at %s: %w", gradleBuildScriptPath, err)
 		} else if !exist {
-			return fmt.Errorf("gradle file not exist at: %s", inputs.GradleFile)
+			return nil, fmt.Errorf("gradle build file not exist at: %s", gradleBuildScriptPath)
 		}
 	}
 
-	return nil
+	gradlewPath := filepath.Join(inputs.ProjectRootDir, "gradlew")
+	if exist, err := pathChecker.IsPathExists(gradlewPath); err != nil {
+		return nil, fmt.Errorf("failed to check if gradlew exist at %s: %w", gradlewPath, err)
+	} else if !exist {
+		return nil, fmt.Errorf("gradlew file not exist at: %s", gradlewPath)
+	}
+
+	taskSlice, err := shellquote.Split(inputs.TestTasks)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse tasks: %w", err)
+	}
+
+	flagSlice, err := shellquote.Split(inputs.GradlewCommandFlags)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse gradlew flags: %w", err)
+	}
+
+	return &Configs{
+		ProjectRootDir:                inputs.ProjectRootDir,
+		GradlewPath:                   gradlewPath,
+		TestTasks:                     taskSlice,
+		GradlewCommandFlags:           flagSlice,
+		GradleBuildScriptRelativePath: inputs.GradleBuildScriptPath,
+	}, nil
 }
 
-func runGradleTask(cmdFactory command.Factory, logger log.Logger, gradleTool, buildFile, tasks, options string) error {
-	optionSlice, err := shellquote.Split(options)
-	if err != nil {
-		return err
-	}
-
-	taskSlice, err := shellquote.Split(tasks)
-	if err != nil {
-		return err
-	}
-
+func runGradleTask(cmdFactory command.Factory, logger log.Logger, workDir string, tasks []string, buildScriptPth string, flags []string) error {
 	var args []string
-	if buildFile != "" {
-		args = append(args, "--build-file", buildFile)
+	if buildScriptPth != "" {
+		args = append(args, "--build-file", buildScriptPth)
 	}
-	args = append(args, taskSlice...)
-	args = append(args, optionSlice...)
+	args = append(args, tasks...)
+	args = append(args, flags...)
 
-	cmd := cmdFactory.Create(gradleTool, args, &command.Opts{
+	cmd := cmdFactory.Create("./gradlew", args, &command.Opts{
 		Stdout: os.Stdout,
 		Stderr: os.Stderr,
+		Dir:    workDir,
 	})
 
 	logger.Donef("$ %s", cmd.PrintableCommandArgs())
